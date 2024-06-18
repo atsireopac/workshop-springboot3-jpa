37d50e02830fafc741f47c06b862a778ab1f5f9c
ERIK BARBOSA DA COSTA <79406452+atsireopac@users.noreply.github.com>
2024-06-18 11:29:47 -0300

    Product

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -10,31 +10,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.Category;
-import com.educandoweb.course.services.CategoryService;
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
-@RequestMapping(value = "/categories")
-public class CategoryResource {
+@RequestMapping(value = "/users")
+public class ProductResource {
 	
 	@Autowired
-	private CategoryService service;
+	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
 	
-	public Category findById(Long id) {
-		Optional<Category> obj = repository.findById(id);
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Category> findById(@PathVariable Long id) {
-		Category obj = service.findById(id);
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
